4d3a152a21d7468c822e3c7b4f6b8d002adbbd78
Henri P Gavin <henri.gavin@duke.edu>
2025-07-20 14:39:03 -0400

    working version with dt = 0.01

--- examples/src/Mathieu_ode_example.c
+++ examples/src/Mathieu_ode_example.c
@@ -11,31 +11,30 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
 
-#include "NRutil.h"
-#include "HPGode.h"
-#include "HPGsignal.h" 
+#include "../../NRutil.h"
+#include "../../HPGode.h"
+#include "../../HPGsignal.h"
+#include "../../HPGmatrix.h"
 
-#define PI 3.14159265358979323846264338327950288419716939937510
-
-
-void Mathieu_sys( float *dzdt, float t, float *x, float *ddu, float *y, float *c)
+void Mathieu_sys(float *dxdt, float t, float *x, float *ui, float *y, float *c)
 // Mathiew_sys
 // compute the state derivitive of a Mathieu nonlinear dynamical system 
-// a pendulum with vertical forcing at the pivot is an example of a Mathieu equation 
+// a pendulum with vertical forcing at the pivot is an example of a Mathieu system 
 {
-  double  cg, cm, cl, cc,            // constants
+  float   cg, cm, cl, ck, cc,        // constants
+          ddu,                       // forcing
           q, dq, du, u,              // states 
-          x1, x2, dx1, dx1,          // coordinates
+          x1, x2, dx1, dx2,          // coordinates
           T, V;                      // energy
 
   // constants
   cg = c[1];                         // gravitational acceleration  m/s^2
   cm = c[2];                         // pendulum mass              kg
   cl = c[3];                         // pendulum length             m
-  cc = c[4];                         // pendulum damping           N.m/(rad/s)
+  ck = c[4];                         // pendulum spring            N.m/rad
+  cc = c[5];                         // pendulum damping           N.m/(rad/s)
 
   // states
   q  = x[1];                         // swing angle, rad
@@ -44,25 +43,25 @@ void Mathieu_sys( float *dzdt, float t, float *x, float *ddu, float *y, float *c
   u  = x[4];                         // pin vertical position
 
   // forcing
-  ddu = u[1];                        // pivot acceleration forcing
+  ddu = ui[1];                       // vertical pivot acceleration 
 
   // analysis
   x1  =  cl * sin(q);                // horizontal position
-  x2  =  cl * cos(q) - c.l + u;      //   vertical position
+  x2  =  cl * cos(q) - cl + u;       //   vertical position
 
   dx1 =  cl * cos(q)*dq;             // horizontal velocity
   dx2 = -cl * sin(q)*dq + du;        //   vertical velocity
 
-  T =  0.5*cm*(dx1*dx1 + dx2*dx2);   //  kinetic  energy
-  V = -cm*cg*x2;                     // potential  energy 
+  T =  0.5*cm*(dx1*dx1 + dx2*dx2);   //    kinetic energy
+  V =  0.5*ck*q*q - cm*cg*x2;        //  potential energy 
 
   // o.d.e's
-  dzdt[1] =  dq; 
-  dzdt[2] = -cm * cl * (cg + ddu) * sin(q) - cc*dq; 
-  dzdt[3] =  ddu; 
-  dzdt[4] =   du;
+  dxdt[1] =  dq; 
+  dxdt[2] = -cm * cl * (cg + ddu) * sin(q) - ck*q - cc*dq; 
+  dxdt[3] =  ddu; 
+  dxdt[4] =   du;
 
-  //  other responses of interest
+  // other responses of interest
   y[1] = T; 
   y[2] = V;
 
@@ -74,72 +73,85 @@ int main (argc,argv)
 int     argc;
 char    *argv[];
 {
-  FILE    *fp;               // output file pointer
- 
-  int    n=4,  m=1, l=2;     // number of states, inputs, outputs
-  int    i,j,k;
+  int    n=4,  m=1, l=2;             // number of states, inputs, outputs
+  int    i,j;
 
   // constants
-  double  cg = 9.81,         // gravitational acceleration  m/s^2
-          cm = 1.0,          // pendulum mass               kg 
-          cl = 1.0,          // pendulum length             m
-          cc = 0.001;        // pendulum damping            N.m/(rad/s)
+  float   cg = 9.81,                 // gravitational acceleration  m/s^2
+          cm = 1.0,                  // pendulum mass               kg 
+          cl = 1.0,                  // pendulum length             m
+          ck = 0.200,                // pendulum stiffness        N.m/rad
+          cc = 0.001;                // pendulum damping          N.m/(rad/s)
 
   // time 
-  double  T  = 220;          // total time duration
-          dt =   0.05;       // time step
-  int     N  = floor(T/dt);  // number of time points
+  float   T  = 200,                  // total time duration         s
+          dt =   0.01;               // time step                   s
+  int     N  = floor(T/dt);          // number of time points
 
   // forcing
-  double  w = 2*sqrt(cg/cl) * 1.00;    // forcing frequency  rad/s
-  double **u, **du, **ddu;       // forcing
+  float   w = 2*sqrt(cg/cl) * 1.00;  // forcing frequency  rad/s
+  float  *u, *du, *ddu, **ui;        // forcing: pin postn, veloc, accel
 
-  double *t;                     // time 
-  double *c;                     // constants
-  double *z0;                    // initial states
-  double **y;                    // output
+  float   *c;                        // constants
+  float   *time;                     // time 
+  float   *x0;                       // initial states
+  float  **x_drv;                    // state solution derivitives
+  float  **x_sol;                    // state solution
+  float  **y;                        // output
 
+  FILE    *fp;                       // output file pointer
+ 
   // memory allocation 
-  c     = dvector(1,4);          // constants
-    u   = dmatrix(1,m,1,N);      // forcing pin displacement 
-   du   = dmatrix(1,m,1,N);      // forcing pin velocity
-  ddu   = dmatrix(1,m,1,N);      // forcing pin acceleration 
-  z0    = dvector(1,n);          // initial state vector
-  z_drv = dmatrix(1,n,1,N);      // state derivitive solution
-  z_sol = dmatrix(1,n,1,N);      // state solution 
-  y     = dmatrix(1,l,1,N);      // output
+  c     = vector(1,5);               // constants
+  time  = vector(1,N);               // time
+     u  = vector(1,N);               // forcing pin displacement 
+    du  = vector(1,N);               // forcing pin velocity
+   ddu  = vector(1,N);               // forcing pin acceleration 
+     ui = matrix(1,m,1,N);           // forcing pin acceleration 
+  x0    = vector(1,n);               // initial state vector
+  x_drv = matrix(1,n,1,N);           // state derivitive solution
+  x_sol = matrix(1,n,1,N);           // state solution 
+  y     = matrix(1,l,1,N);           // output
 
   // constants 
   c[1] = cg; 
   c[2] = cm; 
   c[3] = cl;
-  c[4] = cc;
+  c[4] = ck;
+  c[5] = cc;
 
   // time, s 
-  for(j=1;j<=N;j++) t[j] = (j-1)*dt; 
+  for(j=1;j<=N;j++) time[j] = (j-1)*dt; 
 
   // forcing 
   for(j=1;j<=N;j++) 
-    u[1][j] = 0.200 * cl * sin(w*t[j]) * pow( sin(PI*t[j]/T) , 2 );
+    u[j] = 0.2 * cl * sin(w*time[j]) * pow( sin(PI*time[j]/T) , 2.0 );
    du = cDiff(u,N,dt);
   ddu = cDiff(du,N,dt);
+  for(j=1;j<=N;j++) ui[1][j] = ddu[j];  // input to the ode solver
 
   // initial state
-  z0[1] = 0.01*2*PI;  
-  z0[2] = 0.0;
-  z0[3] = 0.0;
-  z0[4] = 0.0;
+  x0[1] = 0.01*2*PI;   //  q
+  x0[2] = 0.0;         // dq
+  x0[3] = 0.0;         // du
+  x0[4] = 0.0;         //  u
 
 
   // solve the system of o.d.e's 
-  ode4u( Mathieu_sys, time, z0, u, z_drv, z_sol, y, c, N, n,m,l );
+  ode4u( Mathieu_sys, time, x0, ui, x_drv, x_sol, y, c, N, n,m,l );
 
 
   // write results to a file
+  j = 1;
   fp = fopen("Mathieu_sys_data.txt","w"); 
+  fprintf(fp,"%% (1) time\t(2) u\t\t(3) du\t\t(4) ddu\t\t");
+  for(i=1;i<=n;i++) fprintf(fp,"(%d) x_drv_%d\t",    4+i , i ); 
+  for(i=1;i<=n;i++) fprintf(fp,"(%d) x_sol_%d\t",  4+n+i , i ); 
+  for(i=1;i<=l;i++) fprintf(fp,"(%d) y_%d\t",    4+2*n+i , i ); 
+  fprintf(fp,"\n");
   for(j=1;j<=N;j++) {
-    fprintf(fp,"%12.5e\t", t[j] ); 
-    fprintf(fp,"%12.5e\t%12.5e\t%12.5e\t", u[1][j], du[1][j], ddu[1][j] ); 
+    fprintf(fp,"%12.5e\t", time[j] ); 
+    fprintf(fp,"%12.5e\t%12.5e\t%12.5e\t", u[j], du[j], ddu[j] ); 
     for(i=1;i<=n;i++) fprintf(fp,"%12.5e\t", x_drv[i][j] ); 
     for(i=1;i<=n;i++) fprintf(fp,"%12.5e\t", x_sol[i][j] ); 
     for(i=1;i<=l;i++) fprintf(fp,"%12.5e\t", y[i][j] ); 
@@ -149,12 +161,12 @@ char    *argv[];
   fclose(fp);  // close the output data file
 
   // de-allocate memory
-  free_dvector(z0,1,n); 
-  free_dmatrix(x_sol,1,n,1,N);
-  free_dmatrix(x_drv,1,n,1,N);
-  free_dmatrix(u,1,m,1,N);
-  free_dmatrix(du,1,m,1,N);
-  free_dmatrix(ddu,1,m,1,N);
-  free_dmatrix(y,1,l,1,N); 
+  free_vector(  u,1,N);
+  free_vector( du,1,N);
+  free_vector(ddu,1,N);
+  free_vector(x0,1,n); 
+  free_matrix(x_sol,1,n,1,N);
+  free_matrix(x_drv,1,n,1,N);
+  free_matrix(y,1,l,1,N); 
 
 }
